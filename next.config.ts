908a3cd8f0533ae642602be09d5b7018b63d9537
Bruno Tenório <tenoriobn@gmail.com>
2025-06-13 16:49:23 -0300

    feat: :sparkles: 13/06/25 - define idioma padrão e middleware para redirecionamento baseado em cookie

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  i18n: {
+    locales: ['pt_BR', 'es', 'en'],
+    defaultLocale: 'pt_BR',
+    localeDetection: false,
+  },
   compiler: {
     styledComponents: true,
   },
--- src/components/cmsSections/Header/index.tsx
+++ src/components/cmsSections/Header/index.tsx
@@ -1,19 +1,33 @@
 import styled from 'styled-components';
 import {  HeaderProps } from './header.type';
+import Link from 'next/link';
+import { useSetLocale } from 'src/service/postLocale';
 
 const Styled = {
   Header: styled.header`
     color: ${({ theme }) => theme.colors['grey-300']};
   `,
+
+  Language: styled.div`
+    display: flex;
+    gap: 1rem;
+    margin: 3rem 0;
+  `
 };
 
 export default function Header(props: HeaderProps) {
-  // eslint-disable-next-line no-console
-  console.log('header: ', props);
+  const { setLocale } = useSetLocale();
 
   return (
     <header>
       <Styled.Header>Header</Styled.Header>
+      <p>{props.resumeLabel.linkName}</p>
+
+      <Styled.Language>
+        <Link href='/' locale="pt_BR" onClick={() => setLocale('pt_BR')}>Portugues</Link>
+        <Link href='/' locale="es" onClick={() => setLocale('es')}>Espanhol</Link>
+        <Link href='/' locale="en" onClick={() => setLocale('en')}>Inglês</Link>
+      </Styled.Language>
     </header>
   );
 }
