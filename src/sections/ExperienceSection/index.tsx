c93dd5c00e2eaf06393d89fc5c1acf72bdac35c7
Bruno Tenório <tenoriobn@gmail.com>
2025-06-23 11:13:06 -0300

    feat: :sparkles: 23/06/25 - adicionando seção de experiência

--- src/sections/ExperienceSection/index.tsx
+++ src/sections/ExperienceSection/index.tsx
@@ -1,9 +1,54 @@
-import { ExperienceSectionProps } from './experience.type';
+import { useCMSSection } from 'src/hook';
+import { textGradient, Wrapper } from 'src/styles';
+import styled from 'styled-components';
+import ExperienceTimeline from './ExperienceTimeline';
 
-export default function ExperienceSection(props: ExperienceSectionProps) {
-  // eslint-disable-next-line no-console
-  console.log('ExperienceSection: ', props);
+const Styled = {
+  Section: styled.section`
+    display: grid;
+    place-items: center;
+    margin-bottom: 7.5rem;
+    gap: 1rem;
+
+    @media (min-width: 768px) {
+      gap: 1.5rem;
+    }
+  `,
+
+  Title: styled.h2`
+    font-size: 1.5rem;
+    font-weight: 700;
+    text-align: center;
+    ${textGradient}
+
+    @media (min-width: 768px) {
+      font-size: 1.75rem;
+    }
+  `,
+
+  Description: styled.p`
+    font-size: 1rem;
+    text-align: center;
+    max-width: 632px;
+    margin-bottom: .5rem;
+
+    @media (min-width: 768px) {
+      margin-bottom: 2rem;
+    }
+  `,
+};
+
+export default function ExperienceSection() {
+  const { title, description } = useCMSSection('ExperienceSectionBlockRecord');
+
   return (
-    <div></div>
+    <Wrapper>
+      <Styled.Section>
+        <Styled.Title>{title}</Styled.Title>
+        <Styled.Description>{description}</Styled.Description>
+
+        <ExperienceTimeline />
+      </Styled.Section>
+    </Wrapper>
   );
 }
